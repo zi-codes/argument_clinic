db9988a99664353572efeaa42bbbca61101babf4
Student <ziwei.polaris@gmail.com>
2019-07-10 20:59:43 +0100

    refactored with help from Codebar

--- zisim.js
+++ zisim.js
@@ -1,97 +1,80 @@
 // Application to have an argument with you by replacing words in your
 // statements with antonym found via the datamuse API
 
-
 // Selecting page elements
 
-const inputField = document.querySelector('#input');
-const submit = document.querySelector('#submit');
-const responseField = document.querySelector('#responseField');
+const inputField = document.querySelector("#input");
+const submit = document.querySelector("#submit");
+const responseField = document.querySelector("#responseField");
 
 let responseArray = [];
 
-
 // Functions
 
-const getAntonym = (wordQuery,counter,sentenceAsArray) => {
+// Function to clear previous response and displays
+// new response
+const displayResponse = event => {
+  event.preventDefault();
+  responseField.innerHTML = " ";
+  getResponse();
+};
 
-	// AJAX function to fetch antonym for given word from datamuse
-
-	const proxyurl = ""
-	const url = 'https://api.datamuse.com/words?';
-	const queryParams = 'rel_ant=';
-	const endpoint = `${proxyurl}${url}${queryParams}${wordQuery}`;
-	const xhr = new XMLHttpRequest();
-
-	xhr.responseType = 'json';
-	xhr.onreadystatechange = () => {
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-
-			console.log(xhr.response);
-			renderResponse(xhr.response,wordQuery,counter,sentenceAsArray);
-		}
-	}
-
-	xhr.open('GET', endpoint);
-	xhr.send();
-
-}
-
-const displayResponse = (event) => {
-
-	// Function to clear previous response and displays
-	// new response
-
-	event.preventDefault();
-
-	while(responseField.firstChild){
-
-		responseField.removeChild(responseField.firstChild);
-
-	}
-
-	responseArray = []
-
-	getResponse()
-
-}
-
-
+//converts user input into an array and finds antonym for each word
 const getResponse = () => {
-	const sentenceAsArray = inputField.value.split(" ");
-	let counter = 0;
-	sentenceAsArray.forEach(function(wordQuery) {
-		getAntonym(wordQuery,counter,sentenceAsArray)
-		counter++;
-	})
+  const sentenceAsArray = inputField.value.split(" ");
 
-	responseField.innerHTML = responseArray.join(" ")
-}
+  const promisesArray = sentenceAsArray.map(word => getAntonym(word));
 
-const renderResponse = (response,wordQuery,counter,sentenceAsArray) => {
+  Promise.all(promisesArray).then(function(responseAsArray) {
+    const response = decideResponse(responseAsArray, sentenceAsArray);
+    responseField.innerHTML = response;
+  });
+};
 
-  if (response.length === 0) {
-		responseArray[counter] = wordQuery
-	}else{
-		responseArray[counter] = response[0]["word"]
-	}
-	console.log(sentenceAsArray)
-	console.log(responseArray)
+//fetch antonym for given word from datamuse
+const getAntonym = wordQuery => {
+  const proxyurl = "";
+  const url = "https://api.datamuse.com/words?";
+  const queryParams = "rel_ant=";
+  const endpoint = `${url}${queryParams}${wordQuery}`;
 
-	if (JSON.stringify(responseArray) === JSON.stringify(sentenceAsArray)) {
-		alternativeResponse(sentenceAsArray)
-	}else{
-		responseField.innerHTML = responseArray.join(" ")
-	}
-}
+  return fetch(endpoint)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      return renderResponse(myJson, wordQuery);
+    });
+};
 
-const alternativeResponse = (sentenceAsArray) => {
+//decides whether to return the antonym response or a randomised
+//response if the database was unable to find any antonyms
+const decideResponse = (responseAsArray, sentenceAsArray) => {
+  console.log(responseArray);
+  console.log(sentenceAsArray);
+  if (responseAsArray.join(" ") === sentenceAsArray.join(" ")) {
+    return alternativeResponse();
+  } else {
+    return responseAsArray.join(" ");
+  }
+};
 
- const alternativeResponseArray = ["No", "Not so", "I disagree", "Nonsense"]
- const randomResult = Math.floor(Math.random() * alternativeResponseArray.length)
+//chooses the first antonym on the list, or returns original word if
+//non-existent
+const renderResponse = (response, wordQuery) => {
+  return response.length > 0 ? response[0]["word"] : wordQuery;
+};
 
- responseField.innerHTML = alternativeResponseArray[randomResult]
+//chooses a random response from a predefined array in case no
+//antonyms were found
+const alternativeResponse = () => {
+  const alternativeResponseArray = ["No", "Not so", "I disagree", "Nonsense"];
+  const randomResult = Math.floor(
+    Math.random() * alternativeResponseArray.length
+  );
 
-}
+  return alternativeResponseArray[randomResult];
+};
 
-submit.addEventListener('click', displayResponse);
+//event listener
+submit.addEventListener("click", displayResponse);
